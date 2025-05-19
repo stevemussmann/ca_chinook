90a327c98894445a93ba5d9eafb48a5f1c3acaf6
Steve Mussmann <17727337+stevemussmann@users.noreply.github.com>
2025-05-19 16:06:18 -0700

    Add files via upload

---
+++ create_microhaplot_folder.R
@@ -0,0 +1,43 @@
+# redirect output and messages/errors to the log
+log <- file(snakemake@log[[1]], open="wt")
+sink(log, type = "output")
+sink(log, type = "message")
+
+# get the run_dir,  and then create a microhaplot directory
+# there with the Shiny elements in it.
+
+run_dir = snakemake@params$rd
+
+
+inst_packs <- rownames(installed.packages())
+if(!("dplyr" %in% inst_packs)) {
+	message("Installing the remotes package")
+	install.packages("dplyr", repos = "http://cran.rstudio.com")
+}
+if(!("remotes" %in% inst_packs)) {
+	message("Installing the remotes package")
+	install.packages("remotes", repos = "http://cran.rstudio.com")
+}
+#message("Installing microhaplotextract")
+#remotes::install_github(
+#	"eriqande/microhaplot",
+#	ref = "just-for-extracting",
+#	build_vignettes = FALSE,
+#	build_opts = c("--no-resave-data", "--no-manual"),
+#	upgrade = "never" 
+#)
+
+microhaplotextract::mvShinyHaplot(run_dir)
+
+# get rid of the microhaplot example files:
+file.remove(
+	file.path(
+		run_dir, 
+		c(
+			"microhaplot/fish1.rds",
+			"microhaplot/fish1_posinfo.rds",
+			"microhaplot/fish2.rds",
+			"microhaplot/fish2_posinfo.rds"
+		)
+	)
+)
