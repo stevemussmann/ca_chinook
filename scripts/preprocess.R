481cb9e25e35070045167e4f4d0ff1f0b2728a53
Steve Mussmann <17727337+stevemussmann@users.noreply.github.com>
2025-05-20 15:32:39 -0700

    Update preprocess.R

--- scripts/preprocess.R
+++ scripts/preprocess.R
@@ -21,7 +21,7 @@ if (is.null(opt$file)){
 }
 
 # before running this script for the first time, modify the path in the line below so that it points to the location of sample-sheet-processing-functions.R on your computer
-source("/home/mussmann/local/src/microhap/mega-simple-microhap-snakeflow/preprocess/sample-sheet-processing-functions.R")
+source("~/local/src/mega-simple-microhap-snakeflow/preprocess/sample-sheet-processing-functions.R")
 
 create_samples_and_units(opt$file)
 
