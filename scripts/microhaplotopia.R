279718c7974edeb7b763bce2324550883251a5c7
Steve Mussmann <17727337+stevemussmann@users.noreply.github.com>
2025-05-20 15:30:52 -0700

    Add files via upload

--- scripts/microhaplotopia.R
+++ scripts/microhaplotopia.R
@@ -5,66 +5,195 @@
 
 library("tidyverse")
 library("microhaplotopia")
+library("vcfR")
+library("optparse")
+
+## command line option parsing
+option_list = list(
+  make_option(
+    c("-a", "--alleleBalance"), 
+    type="double", 
+    default=0.35, 
+    help="minimum allele balance threshold (default = 0.35)", 
+    metavar="alleleBalance"
+  ),
+  make_option(
+    c("-d", "--hapDepth"), 
+    type="integer", 
+    default=4, 
+    help="minimum haplotype sequencing depth (default = 4)", 
+    metavar="hapDepth"
+  ),
+  make_option(
+    c("-D", "--totDepth"), 
+    type="integer", 
+    default=8, 
+    help="minimum locus sequencing depth (default = 8)", 
+    metavar="totDepth"
+  ),
+  make_option(
+    c("-g", "--greb1rosaOut"), 
+    type="character", 
+    default="greb1rosa_all_hapstr.txt", 
+    help="output file for greb1rosa haplotype string (default = greb1rosa_all_hapstr.txt)", 
+    metavar="greb1rosaOut"
+  ),
+  make_option(
+    c("-G", "--grebInfo"), 
+    type="character", 
+    default="~/local/src/ca_chinook/example_files/greb1_roha_alleles_reordered_wr.txt", 
+    help="info needed to generate greb1rosa haplotype string (default = ~/local/src/ca_chinook/example_files/greb1_roha_alleles_reordered_wr.txt)", 
+    metavar="grebInfo"
+  ),
+  make_option(
+    c("-m", "--microhaplot"), 
+    type="character", 
+    default="microhaplot", 
+    help="microhaplot directory name (default = microhaplot)", 
+    metavar="microhaplot"
+  ),
+  make_option(
+    c("-o", "--output"), 
+    type="character", 
+    default="output", 
+    help="output directory name (default = output)", 
+    metavar="output"
+  ),
+  make_option(
+    c("-r", "--rosa"), 
+    type="character", 
+    default=NULL, 
+    help="ROSA VCF file (No default; required)", 
+    metavar="rosa"
+  ),
+  make_option(
+    c("-R", "--reports"), 
+    type="character", 
+    default="reports", 
+    help="reports directory name (default = reports)", 
+    metavar="reports"
+  ),
+  make_option(
+    c("-s", "--sdy"), 
+    type="character", 
+    default="ordered-read-counts-table.csv", 
+    help="sdy read count file (default = ordered-read-counts-table.csv)", 
+    metavar="sdy"
+  ),
+  make_option(
+    c("-x", "--maxFemale"), 
+    type="integer", 
+    default=2, 
+    help="maximum sdy reads allowed to call female (default = 2)", 
+    metavar="maxFemale"
+  ),
+  make_option(
+    c("-X", "--maxUnknown"), 
+    type="integer", 
+    default=5, 
+    help="maximum number of sdy reads for unknown (default = 5)", 
+    metavar="maxUnknown"
+  )
+);
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$rosa)){
+  print_help(opt_parser)
+  stop("ROSA vcf file must be specified.", call.=FALSE)
+}
 
 # set working directory in Rstudio and capture path in WD
-setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+#setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 WD <- getwd()
 
 ################################################################################
 ## SET FILE AND PATH NAMES HERE
 ################################################################################
 
-# sex ID file name
-sdyReadCounts <- "ordered-read-counts-table.csv"
-
-# file with ROSA haplotype strings (output of makeROSAstring.R)
-rosaStringFile <- "greb1rosa_all_16MAY2025.txt"
-
-# name of file with new column names
-#panelInfo <- "Calif-Chinook-Amplicon-Panel-Information.csv"
-
 # name of folder containing microhaplot output
-microhaplotDir <- "microhaplot"
+microhaplotDir <- opt$microhaplot
+
+# sex ID file name
+sdyReadCounts <- opt$sdy
+
+#your quality and depth-filtered greb1rosa vcf file
+vcfFile <- opt$rosa 
 
 # name of folder where output will be written (will be created if doesn't exist)
-out <- "output"
+out <- opt$output
+outDir <- file.path(WD, out)
+dir.create(outDir, showWarnings = FALSE)
 
 # name of folder where reports will be written (will create if doesn't exist)
-reports <- "reports"
+reports <- opt$reports
+repDir <- file.path(WD, reports)
+dir.create(repDir, showWarnings = FALSE)
+
+# file with ROSA haplotype strings
+rosaStringFile <- file.path(outDir, opt$greb1rosaOut)
+
+# greb locus information file for making hapstring
+grebInfo <- opt$grebInfo
+
+sdyOutFile <- file.path(outDir, "sdy_calls.csv")
 
 ################################################################################
 ## SETTINGS FOR FILTERING
 ################################################################################
 
 # sdy marker settings
-maxFemale <- 2 #maximum number of sdy reads allowed to call female
-maxUnknown <- 5 #maximum number of sdy reads for unknown. Values > maxFemale and <= maxUnknown will be called "Unk" sex. Values > maxUnknown will be called male.
+maxFemale <- opt$maxFemale #maximum number of sdy reads allowed to call female
+maxUnknown <- opt$maxUnknown #maximum number of sdy reads for unknown. Values > maxFemale and <= maxUnknown will be called "Unk" sex. Values > maxUnknown will be called male.
 
 # settings for retaining genotype calls
-hapDepth <- 4 # remove haplotypes with < specified hapDepth
-totDepth <- 8 # remove genotypes with < specified totDepth
-alleleBalance <- 0.35 # heterozygotes only. minimum allowable ratio of read depth for the two haplotypes
+hapDepth <- opt$hapDepth # remove haplotypes with < specified hapDepth
+totDepth <- opt$totDepth # remove genotypes with < specified totDepth
+alleleBalance <- opt$alleleBalance # heterozygotes only. minimum allowable ratio of read depth for the two haplotypes
 
 ################################################################################
-## CREATE DIRECTORIES
+## turn ROSA VCF file into hapstring
 ################################################################################
-# create directory for output files
-outDir <- file.path(WD, out)
-dir.create(outDir, showWarnings = FALSE)
 
-# create directory for reports and summary files
-repDir <- file.path(WD, reports)
-dir.create(repDir, showWarnings = FALSE)
+# read and convert files
+locs <- read.table(file=grebInfo, header=T, sep="\t", stringsAsFactors = F) %>% as_tibble()
+v <- read.vcfR(vcfFile)
+vt <- vcfR2tidy(v)
+
+# Note: E (early), L (late), W (winter), N (non-winter), H (heterozygous) and ? (missing)
+# Extract the individual genotype calls at each SNP
+gtypes <- vt$gt %>% select(ChromKey, POS, Indiv, gt_GT_alleles, gt_DP, gt_AD) %>%
+  left_join(vt$fix %>% select(ChromKey, CHROM, POS)) %>%
+  unite("CHROMPOS", c("CHROM", "POS"), sep="_", remove = T) %>%
+  bind_rows(.id = "vcf_file") %>%
+  filter(CHROMPOS %in% locs$CHROMPOS) %>%
+  group_by(Indiv) %>%
+  arrange(match(CHROMPOS, locs$CHROMPOS), .by_group = TRUE) %>%
+  ungroup()
+
+ptypes <- gtypes %>%
+  select(CHROMPOS, Indiv, gt_GT_alleles) %>%
+  complete(CHROMPOS, Indiv) %>%
+  replace_na(list(gt_GT_alleles = "REF")) %>%
+  left_join(locs) %>%
+  group_by(Indiv) %>%
+  distinct(Indiv, CHROMPOS, .keep_all = TRUE) %>%
+  arrange(haporder, .by_group = TRUE) %>%
+  summarise(hapstr = paste(pheno, collapse = "")) %>%
+  mutate(hapstr = str_replace(hapstr, "NA", "?"))
+#View(ptypes)
+
+ptype_sum <- ptypes %>% group_by(hapstr) %>%
+  summarise(total = n())
+#View(ptype_sum)
+
+write.table(ptypes, file=rosaStringFile, quote=F, sep="\t", row.names=F)
 
 ################################################################################
 ## read ROSA haplotype string file
 ################################################################################
 rosaHapStr <- read.table(file=rosaStringFile, header=TRUE, sep="\t", stringsAsFactors = FALSE) %>% as_tibble()
-
-################################################################################
-## read panel info (contains new column names)
-################################################################################
-#panelInfoStr <- read.table(file=panelInfo, header=TRUE, sep=",", stringsAsFactors = FALSE) %>% as_tibble()
 
 
 ################################################################################
@@ -81,17 +210,15 @@ sdy_out <- sdy %>% mutate(sdy_sex = case_when(
   )) %>% 
   relocate(sdy_sex, .after = sdy_I183)
 sdy_out <- rename(sdy_out, Indiv = NMFS_DNA_ID) # rename NMFS_DNA_ID to Indiv
-# write sdy output
-write_csv(sdy_out, file = file.path(outDir, "sdy_calls.csv"))
-################################################################################
 
+# write sdy output
+write_csv(sdy_out, file = sdyOutFile)
 
 ################################################################################
 ## getting microhaplotypes
 ################################################################################
 hap <- read_unfiltered_observed(file.path(WD, microhaplotDir))
 locCount <- hap %>% group_by(source) %>% summarise(count = n_distinct(locus))
-
 
 # Filter based on depth and allele balance
 hap_fil <- filter_raw_microhap_data(
@@ -115,8 +242,8 @@ hap_fil_nxa <- nxa %>%
   distinct() %>% 
   anti_join(hap_fil, .)
 
-locCount <- hap_fil_nxa %>% group_by(source) %>% summarise(count = n_distinct(locus))
-locCount
+#locCount <- hap_fil_nxa %>% group_by(source) %>% summarise(count = n_distinct(locus))
+#locCount
 
 # Find and drop extra aleles
 xtralleles <- find_contaminated_samples(hap_fil_nxa)
@@ -136,16 +263,15 @@ locus <- xtralleles %>% group_by(locus) %>% summarise(count = n_distinct(indiv.I
 write_csv(source, file = file.path(repDir, "extra_alleles_locus.csv"))
 print(paste("Count of number of individuals with extra alleles per locus written to ", file.path(repDir, "extra_alleles_locus.csv")))
 
-
 # remove genotypes with >2 alleles
 hap_fil1 <- hap_fil_nxa %>% 
   anti_join(xtralleles)
-locCount <- hap_fil1 %>% group_by(source) %>% summarise(count = n_distinct(locus))
-locCount
+#locCount <- hap_fil1 %>% group_by(source) %>% summarise(count = n_distinct(locus))
+#locCount
 
 # these statistics can be examined before/after subsequent filtering steps
 # Check for chinookie
-hap_fil1 %>% filter(str_detect(locus,"^OkiOts")) %>%  pull(haplo) %>% table()
+#hap_fil1 %>% filter(str_detect(locus,"^OkiOts")) %>%  pull(haplo) %>% table()
 
 #Summarize
 loc_depth <- summarize_data(
@@ -167,20 +293,19 @@ grps <- summarize_data(
   datafile = hap_fil1,
   group_var = "group") %>% 
   arrange(., mean_depth)
-grps
+#grps
 
 
 # Plot missing data
 himiss <- calculate_missing_data(hap_fil1)
 hm<-ggplot(data=himiss, aes(x=reorder(indiv.ID, n_miss), y=n_miss)) +
   geom_bar(stat="identity")
-hm
+#hm
 ggsave("missingData.png", path=repDir)
 
 
 # find samples that were removed by filters
 missing_samples <- find_missing_samples(hap, hap_fil1)
-#table(missing_samples$group)
 
 # remove duplicates if necessary
 duplicate_samples <- find_duplicates(hap_fil1)
@@ -236,28 +361,7 @@ haps_2col_final <- full_join(sdy_out,haps_2col_final, by="Indiv") # add ROSA hap
 haps_2col_final <- haps_2col_final %>% select(!c(sdy_I183))
 haps_2col_final <- arrange(haps_2col_final, Indiv) # sort by Indiv column
 
-#View(haps_2col_final)
+# write final genotype file
+write_csv(haps_2col_final, file=file.path(outDir, "haps_2col_final.csv"))
 
-write_csv(haps_2col_final, file="haps_2col_final.csv")
-
-
-
-## FILTERING TO DEAL WITH LATER
-# Remove troublesome loci
-loci2chuck <- c(
-  "tag_id_1872",       #elevated error rate in WHOA!
-  "tag_id_3194",       #elevated error rate in WHOA!
-  "tag_id_1470",       #missing in ~half of individuals
-  "OkiOts_120255-113"    #species ID
-)
-
-hap_fil2 <- filter_bad_loci(
-  long_genos = hap_fil1,
-  bad_loci = loci2chuck
-)
-
-# And  filter himissers 
-hap_fil3 <- filter_missing_data(long_genos = hap_fil2, n_locs = 75)
-
-
-#quit()
+quit()
