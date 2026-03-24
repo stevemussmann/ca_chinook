4e04fb2ff1b1eeafb840b7045589ec89f08e8345
stevemussmann <smussmann@gmail.com>
2026-03-24 16:08:51 -0700

    implement lm for sexID; bunch of new plots

--- scripts/microhaplotopia.R
+++ scripts/microhaplotopia.R
@@ -7,6 +7,11 @@ suppressPackageStartupMessages(library("tidyverse", quietly=TRUE))
 suppressPackageStartupMessages(library("microhaplotopia", quietly=TRUE))
 suppressPackageStartupMessages(library("vcfR", quietly=TRUE))
 suppressPackageStartupMessages(library("optparse", quietly=TRUE))
+suppressPackageStartupMessages(library("reshape2", quietly=TRUE))
+suppressPackageStartupMessages(library("viridis", quietly=TRUE))
+
+# for setting working directory when debugging in Rstudio
+#setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 ## command line option parsing
 option_list = list(
@@ -16,6 +21,13 @@ option_list = list(
     default=0.35, 
     help="minimum allele balance threshold (default = 0.35)", 
     metavar="alleleBalance"
+  ),
+  make_option(
+    c("-c", "--cutoff"), 
+    type="double", 
+    default=0.1, 
+    help="probability cutoff for sex ID calls with regression model (default = 0.1)", 
+    metavar="cutoff"
   ),
   make_option(
     c("-d", "--hapDepth"), 
@@ -49,6 +61,7 @@ option_list = list(
     c("-G", "--grebInfo"), 
     type="character", 
     default="~/local/src/ca_chinook/example_files/greb1_roha_alleles_reordered_wr.txt", 
+    #default="greb1_roha_alleles_reordered_wr.txt", 
     help="info needed to generate greb1rosa haplotype string (default = ~/local/src/ca_chinook/example_files/greb1_roha_alleles_reordered_wr.txt)", 
     metavar="grebInfo"
   ),
@@ -65,6 +78,14 @@ option_list = list(
     default="microhaplot", 
     help="microhaplot directory name (default = microhaplot)", 
     metavar="microhaplot"
+  ),
+  make_option(
+    c("-M", "--modaftc"), 
+    type="character", 
+    default="~/local/src/ca_chinook/example_files/AFTC_model.rds", 
+    #default="AFTC_model.rds", 
+    help=".rds file containing logistic regression model for assigning sexID (default = ~/local/src/ca_chinook/example_files/AFTC_model.rds)", 
+    metavar="modaftc"
   ),
   make_option(
     c("-o", "--output"), 
@@ -91,6 +112,7 @@ option_list = list(
     c("-R", "--run"), 
     type="character", 
     default=NULL, 
+    #default="p133_gt027",
     help="Run number (No default; required)", 
     metavar="run"
   ),
@@ -121,20 +143,6 @@ option_list = list(
     default=10000, 
     help="minimum number of reads per individual for making sex call (default = 10000)", 
     metavar="minReads"
-#  ),
-#  make_option(
-#    c("-x", "--maxFemale"), 
-#    type="integer", 
-#    default=2, 
-#    help="maximum sdy reads allowed to call female (default = 2)", 
-#    metavar="maxFemale"
-#  ),
-#  make_option(
-#    c("-X", "--maxUnknown"), 
-#    type="integer", 
-#    default=5, 
-#    help="maximum number of sdy reads for unknown (default = 5)", 
-#    metavar="maxUnknown"
   )
 );
 
@@ -146,8 +154,7 @@ if (is.null(opt$rosa)){
   stop("ROSA vcf file must be specified.", call.=FALSE)
 }
 
-# set working directory in Rstudio and capture path in WD
-#setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+# capture current working directory in WD
 WD <- getwd()
 
 ################################################################################
@@ -162,6 +169,11 @@ sdyReadCounts <- opt$sdy
 
 #your quality and depth-filtered greb1rosa vcf file
 vcfFile <- opt$rosa 
+#vcfFile <- "CH_gt027_greb1_q20dp5.recode.vcf"
+
+#logistic regression model for making sexID calls (distributed as .rds file)
+modelFile <- opt$modaftc 
+#modelFile <- "AFTC_model.rds"
 
 # name of folder where output will be written (will be created if doesn't exist)
 out <- opt$output
@@ -186,10 +198,9 @@ sdyOutFile <- file.path(outDir, "sdy_calls.csv")
 ################################################################################
 
 # sdy marker settings
+cutoff <- opt$cutoff # probability cutoff for calling sex IDs with logistic regression model. 
 propMale <- opt$propMale # minimum proportion of sdy reads per individual to call male
 minReads <- opt$minReads # minimum number of total reads per individual to make sex call
-#maxFemale <- opt$maxFemale #maximum number of sdy reads allowed to call female
-#maxUnknown <- opt$maxUnknown #maximum number of sdy reads for unknown. Values > maxFemale and <= maxUnknown will be called "Unk" sex. Values > maxUnknown will be called male.
 
 # settings for retaining genotype calls
 hapDepth <- opt$hapDepth # remove haplotypes with < specified hapDepth
@@ -239,14 +250,6 @@ write.table(ptypes, file=rosaStringFile, quote=F, sep="\t", row.names=F)
 ################################################################################
 rosaHapStr <- read.table(file=rosaStringFile, header=TRUE, sep="\t", stringsAsFactors = FALSE) %>% as_tibble()
 
-## old way
-#rosaHapStr <- rosaHapStr %>% mutate(rosa_pheno = case_when(
-#	str_extract(hapstr, "^.{1}") == "E" ~ "sp",
-#	str_extract(hapstr, "^.{1}") == "H" ~ "sp-fall",
-#	str_extract(hapstr, "^.{1}") == "L" ~ "fall",
-#	str_extract(hapstr, "^.{1}") == "?" ~ "unk"
-#))
-
 ## canonical ROSA haplotype classifications
 rosaHapStr <- rosaHapStr %>% mutate(canonical_rosa_pheno = case_when(
 	#Early/Early
@@ -279,17 +282,6 @@ sexy <- read_csv(file=sdyReadCounts, locale=locale(encoding="latin1")) %>% selec
 
 sdy <- bind_rows(sexy)
 
-#sdy_out <- sdy %>% mutate(sdy_sex = case_when(
-#    sdy_I183 <= maxFemale ~ "Female",
-#    sdy_I183 > maxFemale & sdy_I183 <= maxUnknown ~ "Unk",
-#    sdy_I183 > maxUnknown ~ "Male"
-#  )) %>% 
-#  relocate(sdy_sex, .after = sdy_I183)
-#sdy_out <- rename(sdy_out, Indiv = NMFS_DNA_ID) # rename NMFS_DNA_ID to Indiv
-
-# write sdy output
-#write_csv(sdy_out, file = sdyOutFile)
-
 ################################################################################
 ## getting microhaplotypes
 ################################################################################
@@ -319,6 +311,9 @@ locCount <- hap %>% group_by(source) %>% summarise(count = n_distinct(locus))
 ################################################################################
 ## handle sex-ID calls
 ################################################################################
+# read model
+modAFTC <- readRDS(modelFile)
+
 #calculate reads per individual
 readCount <- hap %>% group_by(indiv.ID) %>% summarise(sum_reads = sum(depth))
 readCount <- rename(readCount, Indiv = indiv.ID)
@@ -327,32 +322,44 @@ readCount <- rename(readCount, Indiv = indiv.ID)
 sdy <- rename(sdy, Indiv = NMFS_DNA_ID)
 sdy_out <- left_join(readCount, sdy, by = "Indiv")
 
-#calculate proportion of reads that come from sdy marker
-sdy_out <- sdy_out %>% mutate(prop = sdy_I183 / sum_reads)
-sdy_out <- sdy_out %>% mutate(sdy_sex = case_when(
-		sum_reads >= minReads & prop >= propMale ~ "Male",
-		sum_reads >= minReads & prop < propMale ~ "Female",
+# calculate proportion of reads that come from sdy marker
+sdy_out <- sdy_out %>% mutate(sex_marker_read_prop = sdy_I183 / sum_reads)
+sdy_out <- sdy_out %>% mutate(sdy_prop_sex = case_when(
+		sum_reads >= minReads & sex_marker_read_prop >= propMale ~ "Male",
+		sum_reads >= minReads & sex_marker_read_prop < propMale ~ "Female",
 		.default = NULL
 	)
 )
+
+# classify sex based on logistic regression model
+sdy_out <- sdy_out %>% mutate(prob.male = predict(modAFTC, newdata = sdy_out, type = "response") )
+sdy_out <- sdy_out %>% mutate(sdy_model_sex = case_when(
+  sum_reads >= minReads & prob.male <= cutoff ~ "Female",
+  sum_reads >= minReads & prob.male >= 1.0-cutoff ~ "Male",
+  .default = NULL
+))
 
 # write out sex ID calls to file
 write_csv(sdy_out, file = sdyOutFile)
 
 # count proportion male and female
-sdy_out %>% summarise(prop_male = sum(sdy_sex == "Male", na.rm = TRUE) / n())
-sdy_out %>% summarise(prop_female = sum(sdy_sex == "Female", na.rm = TRUE) / n())
+sdy_out %>% summarise(prop_male = sum(sdy_model_sex == "Male", na.rm = TRUE) / n())
+sdy_out %>% summarise(prop_female = sum(sdy_model_sex == "Female", na.rm = TRUE) / n())
 
 # make scatterplot showing sexID calls by number of reads
-sdyPlot<-ggplot(data=sdy_out, aes(x=sum_reads, y=prop, color = sdy_sex)) +
+sdyPlot<-ggplot(data=sdy_out, aes(x=sum_reads, y=sex_marker_read_prop, color = sdy_model_sex)) +
 	geom_point()
-ggsave("sdyPlot.png", path=repDir)
+ggsave("sdyPlot.png", path=repDir, dpi=600)
 
 # make histogram showing sexID calls by proportion
-sdyHisto<-ggplot(data=sdy_out, aes(x=prop, fill=sdy_sex)) + 
+sdyHisto<-ggplot(data=sdy_out, aes(x=sex_marker_read_prop, fill=sdy_model_sex)) + 
 	geom_histogram(position="identity") + 
 	scale_x_continuous(n.breaks = 10)
-ggsave("sdyHist.png", path=repDir)
+ggsave("sdyHist.png", path=repDir, dpi=600)
+
+readsPlot<-ggplot(data=sdy_out, aes(x=reorder(Indiv, sum_reads), y=sum_reads)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size=2))
+ggsave("readsPlot.png", path=repDir, dpi=600)
 
 
 ################################################################################
@@ -379,15 +386,19 @@ hap_fil_nxa <- nxa %>%
   distinct() %>% 
   anti_join(hap_fil, .)
 
-# Find and drop extra aleles
+# Find and drop extra alleles
 xtralleles <- find_contaminated_samples(hap_fil_nxa)
 write_csv(xtralleles, file = file.path(repDir, "extra_alleles.csv"))
 print(paste("Samples/loci with extra alleles written to ", file.path(repDir, "extra_alleles.csv")))
 
-#And am curious about indiv, run and locus effects
+# And am curious about indiv, run and locus effects
 indiv <- xtralleles %>% group_by(indiv.ID) %>% summarise(count = n_distinct(locus)) #modified from Anthony's code to count the number of loci impacted by extra alleles per individual
 write_csv(indiv, file = file.path(repDir, "extra_alleles_individuals.csv"))
 print(paste("Count of loci per sample impacted by extra alleles written to ", file.path(repDir, "extra_alleles_individuals.csv")))
+
+indivPlot<-ggplot(data=indiv, aes(x=reorder(indiv.ID, count), y=count)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size=2))
+ggsave("extraAllelesIndiv.png", path=repDir, dpi=600)
 
 source <- xtralleles %>% group_by(source) %>% summarise(count = n_distinct(indiv.ID)) #modified from Anthony's code to count the number of individuals per input file that were impacted by extra alleles
 write_csv(source, file = file.path(repDir, "extra_alleles_source.csv"))
@@ -405,32 +416,63 @@ hap_fil1 <- hap_fil_nxa %>%
 # Check for chinookie
 #hap_fil1 %>% filter(str_detect(locus,"^OkiOts")) %>%  pull(haplo) %>% table()
 
-#Summarize
+## Summarize sequencing depth
 loc_depth <- summarize_data(
   datafile = hap_fil1,
   group_var = "locus") %>% 
   arrange(., n_samples)
 write_csv(loc_depth, file = file.path(repDir, "locus_depth.csv"))
-print(paste("Locus depth written to ", file.path(repDir, "locus_depth.csv")))
-
+print(paste("Locus depth summary written to ", file.path(repDir, "locus_depth.csv")))
 
 ind_depth <- summarize_data(
   datafile = hap_fil1,
   group_var = "indiv.ID") %>% 
   arrange(., mean_depth)
 write_csv(ind_depth, file = file.path(repDir, "individual_depth.csv"))
-print(paste("Locus depth written to ", file.path(repDir, "individual_depth.csv")))
+print(paste("Individual depth written to ", file.path(repDir, "individual_depth.csv")))
 
 grps <- summarize_data(
   datafile = hap_fil1,
   group_var = "group") %>% 
   arrange(., mean_depth)
 
-# Plot missing data
-himiss <- calculate_missing_data(hap_fil1)
-hm<-ggplot(data=himiss, aes(x=reorder(indiv.ID, n_miss), y=n_miss)) +
-  geom_bar(stat="identity")
-ggsave("missingData.png", path=repDir)
+## Sequencing Depth Tables
+ind_depth_select <- hap_fil1 %>% select(indiv.ID, locus, depth) # grab relevant columns
+
+# table with total depth
+ind_depth_table_total <- pivot_wider(
+  ind_depth_select,
+  names_from = locus,
+  values_from = depth, values_fn = list(depth = sum),
+)
+print(paste("Depth per locus per individual written to ", file.path(repDir, "totaldepth_per_locus_per_indiv.tsv")))
+write_tsv(ind_depth_table_total,file = file.path(repDir, "totaldepth_per_locus_per_indiv.tsv"))
+
+# plot total depth
+totaldepth_long <- melt(ind_depth_table_total)
+manual_breaks <- c(0, 10, 25, 50, 100, 500, 1000, Inf)
+depth_labels <- c("0-9", "10-24","25-49", "50-99", "100-499", "500-999","1000+")
+totaldepth_long$value_discrete <- cut(totaldepth_long$value, 
+                                      breaks = manual_breaks, 
+                                      labels = depth_labels,
+                                      right = FALSE)
+totaldepth_long$value_discrete <- factor(totaldepth_long$value_discrete)
+totalDepthPlot <- ggplot(totaldepth_long, aes(x = indiv.ID, y = variable, fill = value_discrete)) +
+  geom_tile() +
+  scale_fill_viridis_d(option = "D") + 
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size=1),
+        axis.text.y = element_text(size=4))
+ggsave("totalDepth.png", path=repDir, dpi=1200)
+
+# table with haplotype depths
+ind_depth_table_haplo <- pivot_wider(
+  ind_depth_select,
+  names_from = locus,
+  values_from = depth, values_fn = function(x) paste(x, collapse = "|"),
+)
+print(paste("Depth per haplotype per individual written to ", file.path(repDir, "haplodepth_per_locus_per_indiv.tsv")))
+write_tsv(ind_depth_table_haplo,file = file.path(repDir, "haplodepth_per_locus_per_indiv.tsv"))
 
 # find samples that were removed by filters
 missing_samples <- find_missing_samples(hap, hap_fil1)
@@ -465,18 +507,53 @@ haps_2col_final <- haps_2col_final %>% add_row(indiv=missing_samples$indiv.ID)
 
 haps_2col_final <- rename(haps_2col_final, Indiv = indiv) # rename indiv to Indiv for combining tibbles
 
+# calculate and plot missing data now that missing individuals have been added back
+himiss <- haps_2col_final %>% mutate(n_miss = rowSums(is.na(haps_2col_final)/2)) # count missing loci and add as n_miss field
+himiss <- select(himiss, Indiv, n_miss) # select only Indiv and n_miss fields
+hm<-ggplot(data=himiss, aes(x=reorder(Indiv, n_miss), y=n_miss)) +
+  geom_bar(stat="identity")
+ggsave("missingData.png", path=repDir, dpi=600)
+
 # calculate percent of microhaps genotyped per individual
 percentLoci <- unlist(as_tibble(((ncol(haps_2col_final)-1)-rowSums(is.na(haps_2col_final)))/(ncol(haps_2col_final)-1))*100)
 # add to tibble and move to first column after individual name
 haps_2col_final <- haps_2col_final %>% mutate(percMicroHap = percentLoci)
 haps_2col_final <- haps_2col_final %>% relocate(percMicroHap, .after = Indiv)
 
+## plot depth vs. percMicroHap
+percMicro <- select(haps_2col_final, Indiv, percMicroHap) # get relevant columns
+seqSucc <- left_join(readCount, percMicro, by = c("Indiv" = "Indiv")) # combine relevant columns
+
+# make zoomed-in plot
+print("If the next line is a warning about missing values just ignore it.")
+succPlotZoomed <- ggplot(seqSucc, aes(y=percMicroHap, x=sum_reads)) +
+  # geom_smooth(method = "lm") + 
+  geom_point() + xlim(c(0,50000)) + theme_minimal() + xlab("Total Reads") + ylab("Percent Success") + ggtitle("Success Rate vs. Total Reads (Zoomed)")
+ggsave("success_rate_v_total_reads_zoomed.png", path=repDir, dpi=600)
+
+# plot all individuals
+succPlot <- ggplot(seqSucc, aes(y=percMicroHap, x=sum_reads)) +
+  # geom_smooth(method = "lm") + 
+  geom_point() + theme_minimal() + xlab("Total Reads") + ylab("Percent Success") + ggtitle("Success Rate vs. Total Reads (All)")
+ggsave("success_rate_v_total_reads_all.png", path=repDir, dpi=600)
+
+
+
 # add the sex and ROSA columns
 haps_2col_final <- full_join(rosaHapStr,haps_2col_final, by="Indiv") # add ROSA hap string
 haps_2col_final <- full_join(sdy_out,haps_2col_final, by="Indiv") # add ROSA hap string
-haps_2col_final <- haps_2col_final %>% select(!c(sdy_I183)) # remove extra columns added by sdy_out
-haps_2col_final <- haps_2col_final %>% select(!c(sum_reads)) # remove extra columns added by sdy_out
-haps_2col_final <- haps_2col_final %>% select(!c(prop)) # remove extra columns added by sdy_out
+
+# add extra allele counts
+xtraAlleles <- indiv %>% rename(Indiv = indiv.ID)
+haps_2col_final <- full_join(xtraAlleles,haps_2col_final, by="Indiv")
+haps_2col_final <- haps_2col_final %>% mutate(count = replace_na(count, 0))
+
+# remove extra columns added by sdy_out
+haps_2col_final <- haps_2col_final %>% select(!c(sdy_I183))
+haps_2col_final <- haps_2col_final %>% select(!c(sum_reads))
+haps_2col_final <- haps_2col_final %>% select(!c(sex_marker_read_prop))
+haps_2col_final <- haps_2col_final %>% select(!c(sdy_prop_sex))
+haps_2col_final <- haps_2col_final %>% select(!c(prob.male))
 
 haps_2col_final <- arrange(haps_2col_final, Indiv) # sort by Indiv column
 
@@ -486,19 +563,26 @@ haps_2col_final <- rename(haps_2col_final, indiv = Indiv) # rename indiv to Indi
 # I hate R, and especially tidyverse.
 print("Sorting alleles alphabetically per locus per individual. This part takes longer to run than it probably should.")
 n <- names(haps_2col_final) # get column names
-r <- c("indiv", "sdy_sex", "hapstr", "canonical_rosa_pheno", "percMicroHap") # vector of columns to remove
+r <- c("count", "indiv", "sdy_model_sex", "hapstr", "canonical_rosa_pheno", "percMicroHap") # vector of columns to remove
 result <- setdiff(n, r) # remove columns
 result2 <- str_sub(result, end = -3) # remove _1 and _2 from end of allele names
 loci <- unique(result2) # reduce to only locus names
 
+# calculate proportion of loci that had extra alleles
+haps_2col_final <- haps_2col_final %>% mutate(perc_Xtra = (count/length(loci))*100) # calculate percent loci that had extra alleles
+haps_2col_final <- haps_2col_final %>% relocate(perc_Xtra, .after = percMicroHap) # move location
+haps_2col_final <- arrange(haps_2col_final, indiv) # sort by indiv column
+haps_2col_final <- haps_2col_final %>% select(!c(count)) # remove count column
+
+# do the allele sort
 for (locus in loci) {
   allele1 = paste0(locus, "_1")
   allele2 = paste0(locus, "_2")
   cols <- haps_2col_final %>% select(any_of(c(allele1, allele2)))
   
-  cols2 <- cols |> # what even is this '|>' nonsense?
+  cols2 <- cols |> 
     rowwise() |>
-    mutate(sorted_vals = list(sort(c_across(everything())))) %>% # this %>% is nonsense too.
+    mutate(sorted_vals = list(sort(c_across(everything())))) %>%
     mutate(c1_sorted = sorted_vals[1],
            c2_sorted = sorted_vals[2]
     ) %>%
@@ -509,8 +593,8 @@ for (locus in loci) {
     )
   #print(columns2, n=100)
   
-  haps_2col_final[[allele1]] <- cols2[[allele1]] # and now I have to access the variable a different way.
-  haps_2col_final[[allele2]] <- cols2[[allele2]] # why is it so hard to access a column name by variable?
+  haps_2col_final[[allele1]] <- cols2[[allele1]]
+  haps_2col_final[[allele2]] <- cols2[[allele2]]
   
 }
 print("Done sorting.")
